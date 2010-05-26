7cfcae6edf54fe280d80c00102c67e6588935fe8
fabiod7 <fabiod7@43d2eda8-10bd-2c3f-ad9a-4f8d7119b9f0>
2010-05-26 00:40:14 +0000

    Inicio do projeto

---
+++ [CG]Tetris_3D/src/cg/gl2d/editor/MainWindow.java
@@ -0,0 +1,47 @@
+package cg.gl2d.editor;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+/**
+ * Janela principal do editor de polígonos 2D
+ */
+public class MainWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Cria a janela com a barra de ferramentas e a área de edição
+	 */
+	public MainWindow() {
+		super("Tétris 3D");   
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+		initComponents();
+	}	
+	
+	/**
+	 * Inicializa os componentes da tela
+	 */
+	private void initComponents() {
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new BorderLayout());
+//		contentPane.add(createToolBar(), BorderLayout.NORTH);
+//		contentPane.add(createEditor(), BorderLayout.CENTER);
+	}
+	
+	/**
+	 * Método de inicialização do editor
+	 * @param args Argumentos de inicialização passados pela JVM
+	 */
+	public static void main(String[] args) {
+		MainWindow mainWindow = new MainWindow();
+		mainWindow.setVisible(true);
+	}
+	
+}
