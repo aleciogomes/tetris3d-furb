f6a6c57c46838425598d958404e0b0ff0e13a537
areshio <areshio@43d2eda8-10bd-2c3f-ad9a-4f8d7119b9f0>
2010-06-09 22:31:18 +0000

    Matriz de controle das posições

---
+++ [CG]Tetris_3D/src/cg/gl3d/controle/MatrizControle.java
@@ -0,0 +1,35 @@
+package cg.gl3d.controle;
+
+public class MatrizControle {
+	
+	private int matriz[][];
+	private final int rowCount = 20;
+	private final int columnCount = 10;
+	
+	public MatrizControle(){
+		matriz = new int[rowCount][columnCount];
+		
+		for (int i = 0; i < rowCount; i++) {
+			for (int j = 0; j < columnCount; j++) {
+				matriz[i][j] = 0;
+			}
+		}
+	}
+	
+	public void setStatus(int row, int column, boolean busy){
+		matriz[row][column] = busy ? 1 : 0;
+	}
+	
+	public boolean isBusy(int row, int column){
+		return matriz[row][column] == 1;
+	}
+	
+	public int getLineCount(){
+		return rowCount;
+	}
+	
+	public int getRowCount(){
+		return columnCount;
+	}
+
+}
